7834aa513e717c47824354cf03738e336ed744a7
Adam <adam.krukowski2021@gmail.com>
2024-11-10 21:56:52 +0000

    testing cd

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -24,4 +24,18 @@ RSpec.describe Link, type: :model do
     expect(link.valid?).to be(false)
   end
 
+  it "is invalid if the lookup code already exists" do
+    link = Link.new(
+      original_url: "https://www.favouritewebsite.com/articles/how-to-cook",
+      lookup_code: "1234567"
+    )
+    link.save
+
+    link_2 = Link.new(
+      original_url: "https://www.favouritewebsite.com/articles/how-to-bake",
+      lookup_code: "1234567"
+    )
+    expect(link_2.valid?).to be(false)
+  end
+
 end
