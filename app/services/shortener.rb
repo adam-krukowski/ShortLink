16fac0f8ae9cf6802b4cd4112fa1e60568f19963
Adam <adam.krukowski2021@gmail.com>
2024-11-14 15:44:56 +0000

    all green

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -8,13 +8,9 @@ attr_reader :url, :link_model
   end
 
   def lookup_code
-        #1. get a lookup code.
-        #2. check if it already exists
-        #3. break out of the look if it does
-        #4. if not, get another code
     i = 0
     loop do
-      code = Digest::SHA256.hexdigest(url)[i..(i + 6)]
+      code = get_fresh_code(i)
       break code unless link_model.exists?(lookup_code: code)
       i = i + 1
     end
@@ -22,5 +18,9 @@ attr_reader :url, :link_model
 
   def generate_short_link
     link_model.create!(original_url: url, lookup_code: lookup_code)
+  end
+
+  def get_fresh_code(i)
+    Digest::SHA256.hexdigest(url)[i..(i + 6)]
   end
 end
